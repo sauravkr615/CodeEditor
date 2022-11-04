1d3276ef3744518bf335b4f5695045e2d3527412
Saurav Kumar <iamansh615@gmail.com>
2022-11-04 21:29:37 +0530

    update

--- Projects/WeatherApp/index.js
+++ Projects/WeatherApp/index.js
@@ -1,6 +1,7 @@
-let cityname = document.querySelector('.city-name');
+let cityname = document.querySelector('.City-name');
 let temperature = document.querySelector('.temperature');
 let description = document.querySelector('.Weather-description');
+let weatherIcon = document.querySelector('.weather-icon');
 let inputValue = document.querySelector('.city-search');
 
 inputValue.addEventListener('keyup', function(e){
@@ -11,8 +12,15 @@ inputValue.addEventListener('keyup', function(e){
 })
 
 function getWeatherInfo(name){
-    let api = `https://api.openweathermap.org/data/2.5/weather?lat=26.5778&lon=78.494953&appid=api.openweathermap.org/data/2.5/weather?q=Berlin&APPID=5e3a18f4ff7da54cdf81446a985827c7`;
+    let api = `http://api.openweathermap.org/data/2.5/weather?q=${name}&APPID=5e3a18f4ff7da54cdf81446a985827c7&units=metric`;
     fetch(api)
     .then((Response) => Response.json())
-    .then((data) => console.log(data))
+    .then((data) => updateWeatherInfo(data));
+}
+
+function updateWeatherInfo(data){
+    weatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`
+    cityname.innerHTML = data.name;
+    temperature.innerHTML = ~~data.main.temp;
+    description.innerHTML = `${data.weather[0].description}, Humidity ${data.main.humidity}%`
 }
